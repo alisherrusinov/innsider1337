3478a2d28211c1dd65d4f9cf174e4de96c05aae0
DanteXGit <alisher.nugmanoff@gmail.com>
2020-12-05 01:24:54 +0500

    added models

--- comingsoon/models.py
+++ comingsoon/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+
+class submitmodel(models.Model):
+    text = models.EmailField(default='ivan@gmail.com')
--- comingsoon/views.py
+++ comingsoon/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
-
+from .models import submitmodel
 
 def home(request):
-    context = {}
-    template = 'comingsoon/home.html'
-    return render(request, template, context)
+    if request.method == 'POST':
+        email = request.get('mail')
+        model = submitmodel(text=email)
+        model.save()
+        template = 'comingsoon/home.html'
+        return render(request, template)
+    else:
+        context = {}
+        template = 'comingsoon/home.html'
+        return render(request, template, context)
+
